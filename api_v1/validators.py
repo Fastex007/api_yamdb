93fd2054111b86d392f5d5769dbe125c16b06126
OlepPronkin <fastex007@yandex.ru>
2021-04-30 00:14:01 +0300

    final project. Sprint 11

---
+++ api_v1/validators.py
@@ -0,0 +1,10 @@
+
+import datetime as dt
+
+from django.core.exceptions import ValidationError
+
+
+def date_validator(value):
+    current_date = dt.date.today().year
+    if value > current_date:
+        raise ValidationError('Вы потаетесь созадть title из будущего')
